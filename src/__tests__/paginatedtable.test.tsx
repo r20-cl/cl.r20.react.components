022c0813c08905c7b41d75676540426ec076fa1b
claukers <claukers@amelia>
2020-09-13 19:24:20 -0300

    paginatedtable with tests

---
+++ src/__tests__/paginatedtable.test.tsx
@@ -0,0 +1,116 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as React from "react";
+import {strictEqual} from "assert";
+import {render, RenderResult} from "@testing-library/react";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import {PaginatedEndpointTable, PaginatedEndpointTableProps} from "../paginatedtable";
+import express from "express";
+import {fake} from "@miqro/core";
+
+const renderComponent = (props: PaginatedEndpointTableProps): RenderResult => {
+  return render(<PaginatedEndpointTable {...props} />);
+};
+
+describe("<PaginatedEndpointTable />", () => {
+  let fakeServer = null;
+
+  const fakeModel = fake((req, res) => {
+    req.query.pagination = JSON.parse(req.query.pagination);
+    switch (fakeModel.callCount) {
+      case 1:
+        strictEqual(req.query.pagination.limit, 10);
+        strictEqual(req.query.pagination.offset, 0);
+        res.json({
+          result: {
+            rows: [
+              {
+                id: "bla",
+                name: "ble"
+              }
+            ],
+            count: 1000
+          }
+        });
+        break;
+      case 2:
+        strictEqual(req.query.pagination.limit, 10);
+        strictEqual(req.query.pagination.offset, 1);
+        res.json({
+          result: {
+            rows: [
+              {
+                id: "bla",
+                name: "ble"
+              }
+            ],
+            count: 1000
+          }
+        });
+        break;
+    }
+  });
+  beforeAll(async () => {
+    const app = express();
+    app.get("/mymodel", fakeModel);
+    fakeServer = app.listen(8080);
+  });
+
+  afterAll(async () => {
+    fakeServer.close();
+  });
+
+  test("happy path with offset change from props rerender", async () => {
+    const props = {
+      endpoint: {
+        endpoint: "http://localhost:8080/mymodel",
+        headers: {}
+      },
+      search: {
+        columns: ["id", "name"]
+      },
+      table: {
+        offset: 0,
+        limit: 10,
+        columns: ["name"]
+      },
+      onPageData: fake(() => {
+        // TODO asserts
+      })
+    };
+    const {rerender} = renderComponent(props);
+    await new Promise(resolve => {
+      const interval = setInterval(() => {
+        if (fakeModel.callCount === 1 && props.onPageData.callCount === 1) {
+          clearInterval(interval);
+          resolve();
+        }
+      }, 0);
+    });
+    const newProps = {
+      endpoint: {
+        endpoint: "http://localhost:8080/mymodel",
+        headers: {}
+      },
+      search: {
+        columns: ["id", "name"]
+      },
+      table: {
+        offset: 1,
+        limit: 10,
+        columns: ["name"]
+      },
+      onPageData: fake(() => {
+        // TODO asserts
+      })
+    };
+    rerender(<PaginatedEndpointTable {...newProps} />)
+    await new Promise(resolve => {
+      const interval = setInterval(() => {
+        if (fakeModel.callCount === 2 && newProps.onPageData.callCount === 1 && props.onPageData.callCount === 1) {
+          clearInterval(interval);
+          resolve();
+        }
+      }, 0);
+    });
+  }, 10000);
+});
---
+++ src/paginatedtable.tsx
@@ -0,0 +1,139 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React, {Component} from "react";
+import {v4} from "uuid";
+import {ParseOptionsError, request, RequestResponse, SimpleMap} from "@miqro/core";
+
+export interface PaginatedEndpointTableProps {
+  table: {
+    bodyClassname?: string;
+    headClassname?: string;
+    offset: number;
+    limit: number;
+    className?: string;
+    columnsClassname?: string;
+    columnsTRClassname?: string;
+    columns: string[];
+    rowsClassname?: string;
+    rowsTRClassname?: string;
+  },
+  onError?: (e: Error) => void;
+  onPageData?: (response: RequestResponse) => void;
+  search: {
+    searchQuery?: string,
+    placeHolder?: string;
+    columns: string[];
+  },
+  endpoint: {
+    endpoint: string;
+    headers: SimpleMap<string>;
+  };
+}
+
+export interface PaginatedEndpointTableState {
+  rows: any[];
+  renderedOffset?: number;
+  pageCount: number;
+}
+
+export class PaginatedEndpointTable extends Component<PaginatedEndpointTableProps, PaginatedEndpointTableState> {
+  private unMounted = false;
+
+  constructor(props: PaginatedEndpointTableProps) {
+    super(props);
+    this.state = {
+      rows: [],
+      renderedOffset: undefined,
+      pageCount: 0
+    };
+    this.updatePage = this.updatePage.bind(this);
+  }
+
+  componentDidUpdate(prevProps: PaginatedEndpointTableProps): void {
+    if (prevProps.table.offset !== this.props.table.offset) {
+      this.updatePage();
+    }
+  }
+
+  componentDidMount(): void {
+    this.updatePage();
+  }
+
+  protected updatePage(): void {
+    (async () => {
+      const response = await request({
+        url: `${this.props.endpoint.endpoint}?pagination=${JSON.stringify({
+          limit: this.props.table.limit,
+          offset: this.props.table.offset,
+          search: {
+            columns: this.props.search.columns,
+            query: this.props.search.searchQuery
+          }
+        })}`,
+        headers: this.props.endpoint.headers,
+        method: "GET"
+      });
+      if (response.data.result && response.data.result.rows) {
+        if (!this.unMounted) {
+          this.setState({
+            rows: response.data.result.rows,
+            renderedOffset: this.props.table.offset,
+            pageCount: Math.ceil(response.data.result.count / this.props.table.limit)
+          }, () => {
+            if (this.props.onPageData) {
+              try {
+                this.props.onPageData(response);
+              } catch (e) {
+                console.error(e);
+              }
+            }
+          });
+        }
+      } else {
+        throw new ParseOptionsError("invalid endpoint data");
+      }
+    })().catch((e) => {
+      console.error(e);
+      if (this.props.onError) {
+        try {
+          this.props.onError(e);
+        } catch (e) {
+          console.error(e);
+        }
+      }
+    });
+  }
+
+  componentWillUnmount(): void {
+    this.unMounted = true;
+  }
+
+  protected renderColumns(columns: string[]): JSX.Element {
+    return (
+      <tr key={v4()} className={this.props.table.columnsClassname}>
+        {columns.map(name => <th key={v4()} className={this.props.table.columnsClassname}>{name}</th>)}
+      </tr>
+    )
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  protected renderRow(columns: string[], row: any): JSX.Element {
+    return (
+      <tr key={v4()} className={this.props.table.rowsTRClassname}>
+        {columns.map(columnName => <td key={v4()} className={this.props.table.rowsClassname}>{row[columnName]}</td>)}
+      </tr>
+    )
+  }
+
+  public render(): JSX.Element {
+    return (
+      <table className={this.props.table.className}>
+        <thead className={this.props.table.headClassname}>
+        {this.renderColumns(this.props.table.columns)}
+        </thead>
+        <tbody className={this.props.table.bodyClassname}>
+        {this.state.rows.map(val => this.renderRow(this.props.table.columns, val))}
+        </tbody>
+      </table>
+    );
+  }
+}
